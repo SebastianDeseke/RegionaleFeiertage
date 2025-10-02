30ff608d8b49a175cf70a8eb3d7bab4bafdd8ab5
sebastian <deseke97@gmail.com>
2025-10-02 09:47:31 +0200

    prototype for ics generation

---
+++ src/RegionaleFeiertage.API/Controllers/CalendarFileGeneratorController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using RegionaleFeiertage.Lib;
+
+namespace RegionaleFeiertage.API;
+
+public class CalendareFileGeneratorController : ControllerBase
+{
+    /*in the future I want to make an .ics file
+    generator to make it easy to import anywhere
+    If i want to use Ical.Net:
+    dotnet add package iCal.NET */
+}
